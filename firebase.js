249ca11fdd4a945ed3137bf2a1d1f27c75d1c26f
h2oos <h2oos@proton.me>
2024-08-18 08:36:21 -0400

    configure firebase.js, apikey/authdomain,projectid etc ... added

--- firebase.js
+++ firebase.js
@@ -1,7 +1,13 @@
+// Import the functions you need from the SDKs you need
 import { initializeApp } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore';
 import { getAnalytics } from "firebase/analytics";
 
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
     apiKey: "AIzaSyAD9mdWhF_haifnloyw1sRqT-X6XVOFf-c",
     authDomain: "flashcard-saas-f57ca.firebaseapp.com",
@@ -10,6 +16,9 @@ const firebaseConfig = {
     messagingSenderId: "623640422750",
     appId: "1:623640422750:web:fd41d9443c394fefc887cf",
 };
+
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
 const db = getFirestore(app);
 export default db; 
