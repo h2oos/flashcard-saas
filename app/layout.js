85ae7e63204a70311ec5ab6af3655c0dd5f1fc85
h2oos <h2oos@proton.me>
2024-08-26 10:02:41 -0400

    layout.js complete

--- app/layout.js
+++ app/layout.js
@@ -13,8 +13,8 @@ export default function RootLayout({ children }) {
   return (
     <ClerkProvider>
       <html lang="en">
-      <body className={inter.className}>{children}</body>
-    </html>
+        <body className={inter.className}>{children}</body>
+      </html>
     </ClerkProvider>
   );
 }
